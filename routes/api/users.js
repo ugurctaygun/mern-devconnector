2a1ab7c8aa0fb3ae8d35aa3752bea6a83e94e6ec
ugurctaygun <ugurctaygun@gmail.com>
2020-05-05 14:43:53 +0300

    db schema, authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,57 @@
 const express = require('express');
 const router = express.Router();
+//Express Validator
+const { check, validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register User
 // @access  Public
-router.get('/', (req,res) => {
-    res.send('User route')
+router.post('/', [
+    check('name', 'Name is Required').not().notEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6})
+] , async (req,res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array()});
+    }
+
+    const { name, email, password} = req.body;
+    
+    try {
+        let user = await User.findOne({ email });
+        //check if user already exists
+        if (user) {
+        return res.status(400).json({errors: [ {msg: 'User already exists'} ]});
+        }
+        // get avatar from email using gravata lib
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        });
+        //create new user
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+        //encrypt password using bcrypt lib
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        res.send('user registered'); 
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }   
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,17 @@ const app = express();
 //Connect DB
 connectDB();
 
+//Init middleware
+app.use(express.json({
+    extended: false
+}));
+
 app.get('/', (req,res) => res.send('API Running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
